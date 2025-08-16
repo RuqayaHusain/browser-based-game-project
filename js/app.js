77869b6a27579d2d03237ce58c709a1966e2cb7b
Ruqaya Husain <ruqayanader22@gmail.com>
2025-08-16 18:42:09 +0300

    added all required functionality for the game

--- js/app.js
+++ js/app.js
@@ -13,13 +13,14 @@ let gameStatus; //Win, Loss, or InProgress
 /*------------------------ Cached Element References ------------------------*/
 const puzzleBoardEl = document.querySelector('.puzzle-board');
 const movesCounterEl = document.querySelector('#counter');
-const tileEls = document.querySelectorAll('.tile');
+// const tileEls = document.querySelectorAll('.tile');
 const gameMessageEl = document.querySelector('#game-message');
 const resetBtnEl = document.querySelector('#reset');
 
 /*-------------------------------- Functions --------------------------------*/
 
 const init = () => {
+
     board = getRandomValueArray(); //shuffle puzzle
     movesLeft = maxMoves; //set movesLeft to maxMoves (60)
     gameStatus = 'InProgress'; //initial status = 'InProgress'
@@ -29,6 +30,7 @@ const init = () => {
 };
 
 const render = () => {
+
     puzzleBoardEl.innerHTML = ''; //clears the board
     board.forEach(boardValue => { //generates the tile (div elements) using JS and assigns them a value from the borad array
         
@@ -40,6 +42,7 @@ const render = () => {
         } else { //title with value 9, should be hidden, by giving it a new class
             newTileEl.classList.add('empty-tile');
         }
+            // newTileEl.addEventListener('click', handleClick);
             puzzleBoardEl.appendChild(newTileEl);
         
     });
@@ -48,6 +51,7 @@ const render = () => {
 };
 
 const getRandomValueArray = () => {
+
     let randomValuesArray = [];
     for (let i = 1; i <= numberOfTiles; i++){ //will generate array from [1 ... 9]
         randomValuesArray.push(i);
@@ -58,15 +62,66 @@ const getRandomValueArray = () => {
 };
 
 const updateGameMessage = () => { //checks the game status and updates the gameMessage element accordingly
+
     if (gameStatus === 'Win') {
         gameMessageEl.innerText = 'Congrats! You Have Won 🎉!';
+        resetBtnEl.classList.remove('hide');
+        gameMessageEl.classList.remove('hide');
     } else if (gameStatus === 'Loss') {
         gameMessageEl.innerText = 'Game Over 💀!';
+        resetBtnEl.classList.remove('hide');
+        gameMessageEl.classList.remove('hide');
     } else {
         gameMessageEl.innerText = '';
     }
 };
 
+const isMoveValid = (emptyTileIndex, clickedTileIndex) => {
+
+    /* 
+    if the empty tile and the clicked tile are in the same row, they must be next to each other in columns (column difference = 1)
+        or 
+    if the empty tile and the clicked tile are in the same column, they must be next to each other in rows (row difference = 1)
+     */
+
+    const emptyTileRow = Math.floor(emptyTileIndex / gameSize);
+    const emptyTileCol = emptyTileIndex % gameSize;
+    const clickedTileRow = Math.floor(clickedTileIndex / gameSize);
+    const clickedTileCol = clickedTileIndex % gameSize;
+
+    return (
+        (emptyTileRow === clickedTileRow && Math.abs(emptyTileCol - clickedTileCol) === 1) ||
+        (emptyTileCol === clickedTileCol && Math.abs(emptyTileRow - clickedTileRow) === 1)
+    );
+};
+
+const handleClick = (event) => {
+
+    if (gameStatus !== 'InProgress') return; //get out of the function if gameStatus is not InProgress
+    if (event.target.classList.contains('empty-tile')) return; //get out of the function if user clicked on the empty tile
+
+    const clickedTileValue = parseInt(event.target.textContent); //get the value if the clicked tile
+    const clickedTileIndex = board.indexOf(clickedTileValue); //get the index of the clicked tile
+    const emptyTileIndex = board.indexOf(numberOfTiles); //get the index of empty tile
+
+    if(isMoveValid(emptyTileIndex, clickedTileIndex)) {
+        [board[clickedTileIndex], board[emptyTileIndex]] = [board[emptyTileIndex], board[clickedTileIndex]]; //swap
+        movesLeft --;
+        checkGameStatus(); //checks for win or loss
+        render();
+    }
+};
+
+const checkGameStatus = () => {
+
+  if (board.every((value, index) => value === index + 1)) { //checks if all values are in board array are in sequence
+    gameStatus = 'Win';
+  } else if (movesLeft <= 0) {
+    gameStatus = 'Loss';
+  }
+};
+
 init();
 /*----------------------------- Event Listeners -----------------------------*/
-
+resetBtnEl.addEventListener('click', init);
+puzzleBoardEl.addEventListener('click', handleClick);
